6c9b349dc55789845584bbff109395e04d476def
Hafsa Feroz <hferoz1@depaul.edu>
2025-05-23 10:45:07 -0500

    java docs for LazySingleton class

--- src/main/java/org/example/LazySingleton.java
+++ src/main/java/org/example/LazySingleton.java
@@ -1,14 +1,27 @@
 package org.example;
 
+/**
+ * singleton created only when asked for (Lazy singleton)
+ */
 public class LazySingleton {
     private static LazySingleton[] instances = new LazySingleton[3];
     private int id;
 
+    /**
+     * called once for each ID when the instance is created
+     * @param id ID for the singleton instance
+     */
     private LazySingleton(int id) {
         this.id = id;
         System.out.println("LazySingleton " + id + " instantiated");
     }
 
+    /**
+     * returns ID for that singleton instance
+     * @param id ID of the singleton trying to find
+     * @return instance with that ID
+     * @throws ArrayIndexOutOfBoundsException if ID does not exist
+     */
     public static LazySingleton getInstance(int id) {
         if (instances[id] == null) {
             instances[id] = new LazySingleton(id);
@@ -16,6 +29,9 @@ public class LazySingleton {
         return instances[id];
     }
 
+    /**
+     * @return id of singleton instance
+     */
     public int getId() {
         return this.id;
     }
