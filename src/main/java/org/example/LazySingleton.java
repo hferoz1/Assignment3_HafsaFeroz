3110b14153e794a080b8358ea81f37afab686d2c
Hafsa Feroz <hferoz1@depaul.edu>
2025-05-22 23:58:48 -0500

    LazySingleton class created and implemented methods

--- src/main/java/org/example/LazySingleton.java
+++ src/main/java/org/example/LazySingleton.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class LazySingleton {
+    private static LazySingleton[] instances = new LazySingleton[3];
+    private int id;
+
+    private LazySingleton(int id) {
+        this.id = id;
+        System.out.println("LazySingleton " + id + " instantiated");
+    }
+
+    public static LazySingleton getInstance(int id) {
+        if (instances[id] == null) {
+            instances[id] = new LazySingleton(id);
+        }
+        return instances[id];
+    }
+
+    public int getId() {
+        return this.id;
+    }
 }
