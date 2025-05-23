2ce0a11b90988a0388616b6f8b93f85ed86a8fe6
Hafsa Feroz <hferoz1@depaul.edu>
2025-05-22 23:03:45 -0500

    inital code for EagerSingleton class

--- src/main/java/org/example/EagerSingleton.java
+++ src/main/java/org/example/EagerSingleton.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class EagerSingleton {
+    private static final EagerSingleton[] instances = {
+            new EagerSingleton(0),
+            new EagerSingleton(1),
+            new EagerSingleton(2)
+    };
+
+    private final int id;
+
+    private EagerSingleton(int id) {
+        this.id = id;
+        System.out.println("EagerSingleton " + id + " instantiated");
+    }
+
+    public static EagerSingleton getInstance(int id) {
+        return instances[id];
+    }
+
+    public int getId() {
+        return id;
+    }
 }
